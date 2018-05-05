f636a1d290f6e9de130de9f73eb2224c73c79f9d
AbdelRahman Aref <abdelrahman_aref@hotmail.com>
2018-05-05 13:47:34 +0200

    Initial Commit

---
+++ WebKitFramework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebKitFramework
+//
+//  Created by AbdelRahman Aref on 5/5/18.
+//  Copyright © 2018 AbdelRahman Aref. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
